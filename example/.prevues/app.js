37ad0a263482bd0d580f3e41bb1728afe981f5af
Sasan Farrokh <s.farrokh@alibaba.ir>
2020-10-27 02:20:04 +0330

    feat: docs and build

--- example/.prevues/app.js
+++ example/.prevues/app.js
@@ -1,9 +1,13 @@
 import { createApp, h } from 'vue';
+import { router, RouterView, RouterLink } from './router';
 
 export function createPrevuesApp() {
-    return createApp({
+    const app = createApp({
         render() {
-            return h('span', {}, ['Salam'])
+            return h('span', {}, [h(RouterView)])
         }
     })
+    app.use(router)
+    app.component('RouterLink', RouterLink)
+    return app;
 }
